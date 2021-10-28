e99c0fefbde4a697d34584db916e21d6062e712a
babramian <brianabramian@gmail.com>
2021-10-27 19:02:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const PORT = 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.listen(PORT, () => console.log('server active on PORT ${PORT}'))
+
