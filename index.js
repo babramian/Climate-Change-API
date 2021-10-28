6c3a1da44c4cdfd30f18adeb78335cf0b5bf25e3
babramian <brianabramian@gmail.com>
2021-10-27 19:29:43 -0700

    Scraping multiple news sources for urls on climate change

--- index.js
+++ index.js
@@ -7,39 +7,48 @@ const app = express()
 const newspapers = [
     {
         name: 'thetimes',
-        address: 'https://www.thetimes.co.uk/environment/climate-change'
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
     },
     {
         name: 'guardian',
-        address: 'https://www.theguardian.com/environment/climate-crisis'
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
     },
     {
         name: 'telegraph',
-        address: 'https://www.telegraph.co.uk/climate-change'
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
     }
 ]
+
 const articles = []
 
-app.get('/', (req, res) => {
-    res.json('Welcome to my Climate Change News API')
-})
-
-app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
 
             $('a:contains("climate")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
+
                 articles.push({
                     title,
-                    url
+                    url: newspaper.base + url,
+                    source: newspaper.name
                 })
             })
-            res.json(articles)
-        }).catch((err) => console.log(err))
+        })
+})
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
 })
 
 app.listen(PORT, () => console.log('server active on PORT ${PORT}'))
